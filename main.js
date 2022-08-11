ddba1d168fbdc54f90f7290e57d27481c283914e
Lek1z <ms489123@gmail.com>
2022-08-12 04:02:55 +0700

    HW for lesson3

--- main.js
+++ main.js
@@ -2,121 +2,124 @@
 
 // Задание №1
 
-let a = 1, b = 1, c, d;
-
-/* операции происходят последовательно слево направо, поэтому
-сначала сработает инкремент ++ (переменная а увеличится на 1 и станет a = 2),
-после чего переменной с присвоится значение переменной а, т.е. 2
-*/
-c = ++a;
-alert(c); // 2
-
-/* тут в точности наоборот, т.к. инкремент стоит справа, т.е.
-сначала переменной d присвоится значение переменной b (d = 1), после чего
-b увеличится на 1 и станет b = 2
-*/
-d = b++;
-alert (d); // 1
-
-/* сначала сработает инкремент ++ (переменная а увеличится на 1 и станет a = 3),
-после чего произойдет сложение 2 + 3 = 5 и это значение присвоится
-переменной с (с = 5)
-*/
-c = 2 + ++a;
-alert (c); // 5
-
-/* сначала произойдет сложение 2 + 2 = 4 и это значение присвоится переменной
-d (d = 4), после чего сработает инкремент ++ и b увеличится на 1 (b = 3)
-*/
-d = 2 + b++;
-alert(d); // 4
-
-/*
-т.к. во время выполнения кода программы инкремент был использован по 2 раза
-на каждую переменную, то эти переменные увеличились на 2 (a = 3, b = 3)
-*/
-alert(a); // 3
-alert(b) // 3
+for (let i = 0; i <= 10; i++) {
+    if (i == 0) {
+        console.log(i + ' - это ноль');
+    } else if (i % 2 == 0) {
+        console.log(i + ' - четное число');
+    } else {
+        console.log(i + ' - нечетное число');
+    }
+}
 
 // Задание №2
 
-/* сначала выполнится действие в скобках, a *= 2 то же самое, что и a = a * 2,
-что в результате даст нам 4, после чего произойдет сложение 1 + 4 = 5 и это
-значение присвоится переменной x (x = 5)
-*/
-a = 2;
-let x = 1 + (a *= 2); // 5
+const post = {
+    author: "John", //вывести этот текст
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+                likes: 10,
+                dislikes: 2 //вывести это число
+            }
+        },
+        {
+            userId: 5, //вывести это число
+            userName: "Jane",
+            text: "lorem ipsum 2", //вывести этот текст
+            rating: {
+                likes: 3,
+                dislikes: 1
+            }
+        },
+    ]
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
 
 // Задание №3
 
-a = Number(prompt('Введите число a'));
-b = Number(prompt('Введите число b'));
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
 
-if (isNaN(a) || isNaN(b)) {
-    alert('Вы ввели не числа');
-} else if (a >= 0 && b >= 0) {
-    alert('a - b = ' + (a - b));
-} else if (a < 0 && b < 0) {
-    alert('a * b = ' + (a * b));
-} else alert ('a + b = ' + (a + b));
+products.forEach(el => {
+    el.price *= 0.85;
+    console.log('Цена товара ID = ' + el.id + ' со скидкой 15% = ' + el.price);
+})
 
 // Задание №4
 
-function sum(a, b) {
-    return a + b
-};
+const products1 = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: []
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
 
-function sub(a, b) {
-    return a - b
-};
+let arr1 = products1.filter(el => {
+    if ('photos' in el) {
+        if (el.photos.length > 0) {
+            return el;
+        }
+    }
+});
 
-function multi(a, b) {
-    return a * b
-};
+let arr2 = products1.sort((a, b) => {
+    return a.price - b.price
+});
 
-function div(a, b) {
-    return a / b
-};
-
-console.log(sum(6, 2)); // 8
-console.log(sub(6, 2)); // 4
-console.log(multi(6, 2)); // 12
-console.log(div(6, 2)); // 3
+console.log(arr1);
+console.log(arr2);
 
 // Задание №5
 
-function mathOperation(a, b, operation) {
-    switch(operation) {
-        case '+':
-            console.log(sum(a, b));
-            break;
-        case '-':
-            console.log(sub(a, b));
-            break;
-        case '*':
-            console.log(multi(a, b));
-            break;
-        case '/':
-            console.log(div(a, b));
-            break;
-        default:
-            console.log('Что-то пошло не так');
-    };
-};
+function count (a) {
+    console.log(a);
+}
 
-mathOperation(6, 2, '+'); //8
-mathOperation(6, 2, '-'); //4
-mathOperation(6, 2, '*'); //12
-mathOperation(6, 2, '/'); //3
+for (let i = 0; i < 10; count(i), i++);
 
 // Задание №6
 
-let payment = Number(prompt('Сколько денег вы хотите положить?'));
-
-if (isNaN(payment) || payment <= 0) alert('Некорректная сумма');
-
-if ((payment % 10 == 1) && (Math.floor(((payment % 100) / 10)) != 1)) {
-    alert('Ваша сумма в ' + payment + ' рубль успешно зачислена.');
-} else if (((payment % 10 == 2) || (payment % 10 == 3) || (payment % 10 == 4)) && (Math.floor(((payment % 100) / 10)) != 1)) {
-    alert('Ваша сумма в ' + payment + ' рубля успешно зачислена.');
-} else alert('Ваша сумма в ' + payment + ' рублей успешно зачислена.');
+for (let i = 1; i <= 20; i++) {
+    console.log('x'.repeat(i));
+}
